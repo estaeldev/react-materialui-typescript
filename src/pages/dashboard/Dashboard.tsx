08499ac253b9fb980aa9b6eea962d5580088833c
estaeldev <estael.dev@gmail.com>
2023-11-02 09:56:04 -0300

    feat: adicionando recursos na pagina de dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,81 +1,96 @@
-import { Theme, useMediaQuery } from "@mui/material"
-import { FC, useCallback } from "react"
-import { FerramentasDaListagem, FerramentasDeDetalhe } from "../../shared/components"
-import { FerramentasDaListagemButton, FerramentasDaListagemTextField } from "../../shared/components/ferramentas-da-listagem/components"
-import { FerramentasDeDetalheButton } from "../../shared/components/ferramentas-de-detalhe/components"
+import { Box, Card, CardContent, CircularProgress, Grid, Typography } from "@mui/material"
+import { FC, useEffect, useState } from "react"
+import { FerramentasDaListagem } from "../../shared/components"
 import { LayoutBase } from "../../shared/layouts"
+import { CidadesService, PessoasService } from "../../shared/services"
 
 export const Dashboard: FC = () => {
 
-    const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("md"))
-    const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"))
-
-    const handleClickSalvar = useCallback(() => {
-        
-    }, [])
-
-    const handleClickSalvarEVoltar = useCallback(() => {
-        
-    }, [])
-
-    const handleClickApagar = useCallback(() => {
-        
-    }, [])
-
-    const handleClickNovo = useCallback(() => {
-        
-    }, [])
-
-    const handleClickVoltar = useCallback(() => {
-        
-    }, [])
+    const [isLoadingCidades, setIsLoadingCidades] = useState(false)
+    const [isLoadingPessoas, setIsLoadingPessoas] = useState(false)
+    const [totalCidades, setTotalCidades] = useState(0)
+    const [totalPessoas, setTotalPessoas] = useState(0)
     
+    useEffect(() => {
+        setIsLoadingCidades(true)
+        CidadesService.getAll(1).then(result => {
+            setIsLoadingCidades(false)
+
+            if(result instanceof Error) {
+                alert(result.message)
+                return
+            }
+
+            setTotalCidades(result.totalCount)
+
+        })
+        
+        setIsLoadingPessoas(true)
+        PessoasService.getAll(1).then(result => {
+            setIsLoadingPessoas(false)
+            if(result instanceof Error) {
+                alert(result.message)
+                return
+            }
+
+            setTotalPessoas(result.totalCount)
+        })
+            
+
+    }, [])
 
     return (
         <LayoutBase titulo="Página Inicial">
 
-            <FerramentasDaListagem>
-                <FerramentasDaListagemTextField />
-                <FerramentasDaListagemButton />
-            </FerramentasDaListagem>
+            <FerramentasDaListagem />
 
-            <FerramentasDeDetalhe >
+            <Box width="100%" display="flex">
+                <Grid container direction="column" margin={2}>
 
-                <FerramentasDeDetalheButton  
-                    label="SALVAR" 
-                    iconName="save" 
-                    variant="contained" 
-                    handleClick={handleClickSalvar}
-                />
-                <FerramentasDeDetalheButton  
-                    label="SALVAR E FECHAR" 
-                    iconName="save" 
-                    handleClick={handleClickSalvarEVoltar}
-                    isEsconder={mdDown}
-                />
-                <FerramentasDeDetalheButton  
-                    label="APAGAR" 
-                    iconName="delete" 
-                    handleClick={handleClickApagar}
-                    startDivider
-                />
-                <FerramentasDeDetalheButton  
-                    label="NOVO" 
-                    iconName="add" 
-                    handleClick={handleClickNovo} 
-                    isEsconder={smDown}
-                />
+                    <Grid container item direction="row" spacing={2}> 
+                        <Grid item xs={12} md={6} xl={4}> 
+                            <Card >
+                                <CardContent>
+                                    {isLoadingPessoas ? (
+                                        <Typography variant="h4" align="center">
+                                            <CircularProgress />
+                                            Carregando...
+                                        </Typography>
+                                        ) : (
+                                        <>
+                                            <Typography variant="h4" align="center">
+                                                <CircularProgress />
+                                                Total de Pessoas
+                                            </Typography>
+                                            <Box padding={6} display="flex" justifyContent="center" alignItems="center">
+                                                <Typography variant="h1">{totalPessoas}</Typography>
+                                            </Box>
+                                        </>
+                                    )}
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} md={6} xl={4}> 
+                            <Card>
+                                <CardContent>
+                                    {isLoadingCidades ? (
+                                        <Typography variant="h4" align="center">Carregando...</Typography>
+                                        ) : (
+                                        <>
+                                            <Typography variant="h4" align="center">Total de Cidades</Typography>
+                                            <Box padding={6} display="flex" justifyContent="center" alignItems="center">
+                                                <Typography variant="h1">{totalCidades}</Typography>
+                                            </Box>
+                                        </>
+                                    )}
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    </Grid>
 
-                <FerramentasDeDetalheButton  
-                    label="VOLTAR" 
-                    iconName="arrow_back" 
-                    handleClick={handleClickVoltar}
-                    startDivider
-                />
+                </Grid>
+            </Box>
 
-            </FerramentasDeDetalhe>
-
-            Testando
         </LayoutBase>
     )
 
